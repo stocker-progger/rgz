d5ea5c0feb879cf34ec75290cdb29a0bc776f452
Данил Трохин <tdv2004t@mail.ru>
2024-12-24 20:20:18 +0700

    РГЗ

--- app.py
+++ app.py
@@ -1,13 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask import Flask, render_template, request, redirect, url_for, session, abort
 from flask_sqlalchemy import SQLAlchemy
 import hashlib
 import os
-from db import db  
+from db import db
 from db.models import User, Product, Cart
-from os import path  
+from os import path
 
-app = Flask(__name__)
+app = Flask(__rgz__)
 
+# Конфигурация приложения
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
 
@@ -18,31 +19,43 @@ if app.config['DB_TYPE'] == 'postgres':
     host_ip = '127.0.0.1'
     host_port = 5432
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = \
-        f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{host_ip}:{host_port}/{db_name}'
 else:
     dir_path = path.dirname(path.realpath(__file__))
     db_path = path.join(dir_path, "rgz_db_orm.db")
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{db_path}'
 
-db.init_app(app) 
+db.init_app(app)
+
+
+# Вспомогательная функция для проверки авторизации
+def get_current_user():
+    if 'user_id' not in session:
+        abort(403)  # Доступ запрещен
+    return User.query.get(session['user_id'])
+
 
 @app.route('/')
 def index():
     products = Product.query.all()
     return render_template('index.html', products=products)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form.get('username')
         password = request.form.get('password')
-
         user = User.query.filter_by(username=username).first()
+
         if user and user.password == hash_password(password):
             session['user_id'] = user.id
             return redirect(url_for('index'))
+
+        return render_template('login.html', error='Неверное имя пользователя или пароль')
+
     return render_template('login.html')
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -50,86 +63,71 @@ def register():
         username = request.form.get('username')
         password = request.form.get('password')
 
-        existing_user = User.query.filter_by(username=username).first()
-        if existing_user:
+        if User.query.filter_by(username=username).first():
             return render_template('register.html', error='Пользователь с таким именем уже существует')
 
-        else:
-            hashed_password = hash_password(password)
-            user = User(username=username, password=hashed_password)
-            db.session.add(user)
-            db.session.commit()
-            return redirect(url_for('login'))
-        
+        user = User(username=username, password=hash_password(password))
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+
     return render_template('register.html')
+
 
 @app.route('/logout')
 def logout():
     session.pop('user_id', None)
     return redirect(url_for('index'))
+
+
 @app.route('/cart')
 def cart():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
-
-    cart_items = db.session.query(Cart, Product).join(Product, Cart.product_id == Product.id).filter(Cart.user_id == session['user_id']).all()
-
-    cart_with_products = []
-    for cart_item, product in cart_items:
-        cart_with_products.append({
-            'id': cart_item.id,
-            'product_name': product.name,
-            'product_price': product.price,
-            'quantity': cart_item.quantity
-        })
-
-    return render_template('cart.html', cart_items=cart_with_products)
+    user = get_current_user()
+    cart_items = db.session.query(Cart, Product).join(Product).filter(Cart.user_id == user.id).all()
+    return render_template('cart.html', cart_items=cart_items)
 
 
 @app.route('/add_to_cart/<int:product_id>', methods=['POST'])
 def add_to_cart(product_id):
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
-    
-    product = Product.query.get_or_404(product_id)
-    cart_item = Cart.query.filter_by(user_id=session['user_id'], product_id=product.id).first()
+    user = get_current_user()
+    cart_item = Cart.query.filter_by(user_id=user.id, product_id=product_id).first()
+
     if cart_item:
         cart_item.quantity += 1
     else:
-        cart_item = Cart(user_id=session['user_id'], product_id=product.id, quantity=1)
+        cart_item = Cart(user_id=user.id, product_id=product_id, quantity=1)
         db.session.add(cart_item)
+
     db.session.commit()
     return redirect(url_for('cart'))
 
 
 @app.route('/remove_from_cart/<int:cart_item_id>', methods=['POST'])
 def remove_from_cart(cart_item_id):
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
+    user = get_current_user()
+    cart_item = Cart.query.filter_by(id=cart_item_id, user_id=user.id).first()
 
-    cart_item = Cart.query.filter_by(id=cart_item_id, user_id=session['user_id']).first()
     if cart_item:
         db.session.delete(cart_item)
         db.session.commit()
 
     return redirect(url_for('cart'))
 
+
 @app.route('/checkout')
 def checkout():
-    if 'user_id' not in session:
-        return redirect(url_for('login'))
+    user = get_current_user()
+    cart_items = Cart.query.filter_by(user_id=user.id).all()
+    total = sum(Product.query.get(item.product_id).price * item.quantity for item in cart_items)
 
-    cart_items = Cart.query.filter_by(user_id=session['user_id']).all()
-    total = sum(
-        Product.query.get(item.product_id).price * item.quantity for item in cart_items if Product.query.get(item.product_id)
-    )
-    db.session.query(Cart).filter_by(user_id=session['user_id']).delete()
+    Cart.query.filter_by(user_id=user.id).delete()
     db.session.commit()
-    return render_template('checkout.html', total=total)
 
+    return render_template('checkout.html', total=total)
 
 def hash_password(password):
     return hashlib.sha256(password.encode()).hexdigest()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
